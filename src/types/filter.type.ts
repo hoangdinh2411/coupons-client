0b5b42fb6a18cf99893fd6ec6ed70637c8a5b244
William Dinh <hoangdinh2411@gmail.com>
2025-06-28 19:34:58 +0200

    add prettier-plugin-tailwindcss

--- src/types/filter.type.ts
+++ src/types/filter.type.ts
@@ -1,10 +1,10 @@
 export interface FilterPayload {
-  categories?: number[];
-  stores?: number[];
-  status?: number[];
-  topics?: number[];
-  search_text: string;
-  page: number;
-  is_verified?: boolean;
-  rating?: number;
+  categories?: number[]
+  stores?: number[]
+  status?: number[]
+  topics?: number[]
+  search_text: string
+  page: number
+  is_verified?: boolean
+  rating?: number
 }
