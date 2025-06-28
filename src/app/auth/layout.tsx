610c8fc6745ff4846f7b9406357053bf132669d0
William Dinh <hoangdinh2411@gmail.com>
2025-06-28 19:36:41 +0200

    add ImageTYpe interface

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -9,11 +9,11 @@ export default function AuthLayout({
   children: ReactNode
 }): JSX.Element {
   return (
-    <div className="fixed top-0 left-0 w-full h-full  flex flex-col ">
-      <header className="w-full bg-green flex justify-center items-center p-2 h-auto">
+    <div className="fixed top-0 left-0 flex h-full w-full flex-col">
+      <header className="bg-green flex h-auto w-full items-center justify-center p-2">
         <Link
           href={APP_ROUTERS.INDEX}
-          className="relative w-40 aspect-auto h-12"
+          className="relative aspect-auto h-12 w-40"
         >
           <Image
             src="/images/logo-with-text-and-black-logo.png"
@@ -25,10 +25,10 @@ export default function AuthLayout({
           />
         </Link>
       </header>
-      <main className="flex-1 flex justify-between items-center w-full">
+      <main className="flex w-full flex-1 items-center justify-between">
         {children}{' '}
       </main>
-      <footer className="p-5 text-center border-1 border-l-0 border-r-0 border-b-0 border-gray-300">
+      <footer className="border-1 border-r-0 border-b-0 border-l-0 border-gray-300 p-5 text-center">
         © 2025 Trust Coupon, Inc.
       </footer>
     </div>
