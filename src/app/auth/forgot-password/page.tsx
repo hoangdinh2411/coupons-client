0142a3e32c198e361c65d9f6eaa8446cbcec42de
Quindart <quang.le@aegona.com>
2025-06-29 09:49:51 +0700

    fix: UI form, styled class

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -7,12 +7,6 @@ import { useRouter } from 'next/navigation'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
-const textFieldClass =
-  '!border !border-slate-300 w-full px-2 h-11 rounded-none text-slate-600 mt-1'
-
-const buttonPrimaryClass =
-  'rounded-3xl mt-4 cursor-pointer hover:bg-light-green text-center py-2 text-white hover:text-slate-600 font-bold bg-green'
-
 function ForgotEmailTemplate() {
   const navigation = useRouter()
   const {
@@ -28,48 +22,48 @@ function ForgotEmailTemplate() {
     navigation.push(APP_ROUTERS.VERIFY)
   }
   return (
-    <div className="w-full py-10 max-w-screen h-full flex  flex-col bg-white md:bg-[#F3F4F6] gap-5 justify-center items-center  md:w-[330px] md:h-[500px] md:mx-auto ">
-      <div className="sm:mx-auto w-full sm:w-sm md:mx-1 md:w-full">
-        <div className="md:mb-4">
-          <h3 className="text-center text-[36px] font-semibold">
+    <div className="flex h-full w-full flex-col gap-1 bg-white px-6 pt-6 sm:mx-auto sm:h-[500px] sm:w-[358px] sm:bg-[#F3F4F6] sm:px-0 md:mt-6 md:pt-0">
+      <div className="w-full sm:mx-auto md:w-full">
+        <div className="md:mb-2">
+          <h3 className="text-center text-[32px] font-semibold">
             Forgot Password
           </h3>
         </div>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className=" bg-white w-full gap-2 flex flex-col p-6 rounded-sm"
+          className="flex w-full flex-col max-w-[358px] mx-auto gap-2 rounded-sm sm:bg-[#F3F4F6] md:bg-white md:p-6"
         >
-          <p className="text-center text-slate-500 mb-6">
+          <p className="mt-2 mb-4 text-center text-slate-800">
             Enter your email address below and we&apos;ll send you reset
             instructions.
           </p>
           <div className="form-group w-full">
             <label
               htmlFor="email-input"
-              className="text-slate-800 form-label text-base font-bold block "
+              className="form-label block text-base font-bold text-slate-800"
             >
               Email Address
             </label>
             <input
               id="email-input"
               placeholder="Email Address"
-              className={textFieldClass}
+              className="textfield"
               {...register('email')}
             />
             {errors.email && (
-              <small className="text-red-600 font-500 text-sm mt-2 block">
+              <small className="font-500 mt-2 block text-sm text-red-600">
                 {errors.email.message}
               </small>
             )}
           </div>
 
-          <button type="submit" className={buttonPrimaryClass}>
+          <button type="submit" className="btn-primary">
             {' '}
-            Send
+            Send Instructions
           </button>
           <Link
             href={APP_ROUTERS.SIGN_IN}
-            className="text-green font-500 text-center hover:text-green mt-2"
+            className="text-green font-500 hover:text-green mt-2 text-center"
           >
             Go Back to Login
           </Link>
--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -2,16 +2,9 @@
 import { verifyFormSchema } from '@/helper/auth.validation.schema'
 import { APP_ROUTERS } from '@/helpers/config'
 import { zodResolver } from '@hookform/resolvers/zod'
-import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 import { useForm } from 'react-hook-form'
-const textFieldClass =
-  '!border !border-slate-300 w-full px-2 h-11 rounded-none text-slate-600 mt-1'
-
-const buttonPrimaryClass =
-  'rounded-3xl w-full mt-4 cursor-pointer hover:bg-light-green text-center py-2 text-white hover:text-slate-600 font-bold bg-green'
-
 function VerifyTemplate() {
   const navigation = useRouter()
   const {
@@ -27,46 +20,40 @@ function VerifyTemplate() {
     navigation.push(APP_ROUTERS.INDEX)
   }
   return (
-    <div className="flex flex-col gap-5 mx-auto md:max-w-sm  w-screen md:w-[350px]  bg-white md:bg-[#F3F4F6]">
-      <p className="text-3xl font-bold text-slate-800 mt-10 text-center">
+    <div className="mx-auto flex w-screen flex-col gap-5 bg-white sm:w-[320px] sm:max-w-sm sm:bg-[#F3F4F6]">
+      <p className="mt-6 text-center text-[40px] font-bold text-slate-800">
         Verify Account
       </p>
       <form
-        className=" md:mx-auto px-6 h-screen flex flex-col md:h-[300px] bg-white"
+        className="mx-auto flex h-screen max-w-[358px] flex-col bg-white px-6 sm:mx-auto sm:max-h-[240px]"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <p className="text-center leading-5 mt-4 text-slate-600 mb-6">
+        <p className="mb-6 text-center leading-5 text-slate-600 sm:mt-4">
           Enter your verification code below and we&apos;ll send you reset
           instructions.
         </p>
         <label
           htmlFor="Verification-input"
-          className="text-slate-800 form-label  text-base font-bold block "
+          className="form-label block text-base font-bold text-slate-800"
         >
           Verification code
         </label>
         <input
           id="Verification-input"
           placeholder="Verification code"
-          className={textFieldClass}
+          className="textfield"
           {...register('verificationCode')}
         />
         {errors.verificationCode && (
-          <small className="text-red-600 font-500 text-sm mt-2 block">
+          <small className="font-500 mt-2 block text-sm text-red-600">
             {errors.verificationCode.message}
           </small>
         )}
-        <div className="mt-4"></div>
-        <button type="submit" className={buttonPrimaryClass}>
+        <div className="mt-2"></div>
+        <button type="submit" className="btn-primary">
           {' '}
           Verify
         </button>
-        <Link
-          href={APP_ROUTERS.SIGN_IN}
-          className="text-green font-600 text-center mt-2"
-        >
-          Go back to login
-        </Link>
       </form>
     </div>
   )
