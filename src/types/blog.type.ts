e08fe55575d60cc63ce22d22cc7ea0dc5e64b28b
Quindart <quang.le@aegona.com>
2025-06-29 08:52:47 +0700

    feat: typing

--- src/types/blog.type.ts
+++ src/types/blog.type.ts
@@ -1,27 +1,27 @@
-import { ImageType } from '@/shared/layouts-components/uploadFile/UploadFile';
-import { MetaData, TimestampedEntity } from './share.type';
-import { TopicData } from './topic.type';
+import { ImageType } from '@/shared/layouts-components/uploadFile/UploadFile'
+import { MetaData, TimestampedEntity } from './share.type'
+import { TopicData } from './topic.type'
 
 export interface BlogPayload {
-  title?: string;
-  content?: string;
-  keywords?: string[];
-  topic_id?: number;
-  rating?: number;
-  images?: ImageType;
-  meta_data?: MetaData;
+  title?: string
+  content?: string
+  keywords?: string[]
+  topic_id?: number
+  rating?: number
+  images?: ImageType
+  meta_data?: MetaData
 }
 export interface BlogData extends TimestampedEntity {
-  title: string;
-  content: string;
-  keywords: string[];
-  topic_id: number;
-  id: number;
-  slug: string;
-  topic?: TopicData;
-  meta_data?: MetaData;
-  rating: number;
-  image: ImageType;
-  is_published: boolean;
-  is_indexed: boolean;
+  title: string
+  content: string
+  keywords: string[]
+  topic_id: number
+  id: number
+  slug: string
+  topic?: TopicData
+  meta_data?: MetaData
+  rating: number
+  image: ImageType
+  is_published: boolean
+  is_indexed: boolean
 }
--- src/types/topic.type.ts
+++ src/types/topic.type.ts
@@ -1,13 +1,13 @@
-import { ImageType } from '@/shared/layouts-components/uploadFile/UploadFile';
-import { MetaData } from './share.type';
+import { ImageType } from '@/shared/layouts-components/uploadFile/UploadFile'
+import { MetaData } from './share.type'
 
 export interface TopicPayload {
-  name: string;
-  image: ImageType;
-  meta_data?: MetaData;
+  name: string
+  image: ImageType
+  meta_data?: MetaData
 }
 
 export interface TopicData extends TopicPayload {
-  id: number;
-  slug: string;
+  id: number
+  slug: string
 }
