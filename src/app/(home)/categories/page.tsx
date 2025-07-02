b1b997e49359ff0ffaebf2f1f90606470a60808a
William Dinh <hoangdinh2411@gmail.com>
2025-07-02 10:22:59 +0200

    responsive text size for categories page

--- src/app/(home)/categories/page.tsx
+++ src/app/(home)/categories/page.tsx
@@ -32,17 +32,17 @@ const TOP_CATEGORIES = [
 async function CouponPage() {
   return (
     <div className="container mx-auto w-full max-w-[1280px] p-8">
-      <h2 className="mb-4 text-2xl font-bold sm:text-2xl">
+      <h2 className="mb-4 text-xl font-bold md:text-2xl">
         Top Coupons & Deals Categories
       </h2>
-      <div className="my-9 pb-9 flex flex-wrap items-center gap-1 gap-y-4  border-b-1 border-solid border-gray-200">
+      <div className="my-9 flex flex-wrap items-center gap-1 gap-y-4 border-b-1 border-solid border-gray-200 pb-9">
         <div className="grid w-full grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           {TOP_CATEGORIES.map((category, index) => (
             <CardCoupon {...category} key={index} />
           ))}
         </div>
       </div>
-      <h2 className="text-[20px] font-[900] sm:text-2xl mb-4">
+      <h2 className="mb-4 text-xl font-[900] md:text-2xl">
         All Coupons & Deals Categories
       </h2>
       <Suspense>
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,9 +2,9 @@ import SpinnerLoading from '@/components/loading'
 import React from 'react'
 
 export default function Loading() {
-  return(
-    <div className='fixed top-0 left-0 size-full flex justify-center items-center'>
-       <SpinnerLoading />
+  return (
+    <div className="fixed top-0 left-0 flex size-full items-center justify-center">
+      <SpinnerLoading />
     </div>
   )
 }
