bcf98d8f927fa05963895719eaca1e21d8b9ec4e
Quindart <quang.le@aegona.com>
2025-07-04 21:36:04 +0700

    feat: coupon layout

---
+++ src/components/badge/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import React from 'react'
+type BadgePropsType = {
+  imageIcon?: string
+  text?: string
+  className?: string
+}
+function Badge({ imageIcon, text = '', className = '' }: BadgePropsType) {
+  return (
+    <div
+      className={` ${className} text-base flex rounded-md border-1 pr-2 border-gray-200 bg-white  py-[2px] `}
+    >
+      <Image src={`${imageIcon}`} alt="" width={20} height={20} />
+      <span className="font-bold">{text}</span>
+    </div>
+  )
+}
+
+export default Badge
