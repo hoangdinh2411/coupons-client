fdd9816f9478b881b5589214bfa409fc2bed015b
William Dinh <hoangdinh2411@gmail.com>
2025-07-06 08:55:13 +0200

    re-style for footer

---
+++ src/components/footer/LeftFooter.tsx
@@ -0,0 +1,80 @@
+import { APP_ROUTERS } from '@/helpers/config'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { FaFacebook, FaInstagram, FaPinterest, FaTiktok } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6'
+
+const BOTTOM_FOOTER_DATA = [
+  {
+    href: 'https://www.facebook.com/TrustCoupon',
+    ariaLabel: 'TrustCoupon on Facebook',
+    icon: FaFacebook,
+  },
+  {
+    href: 'https://www.instagram.com/TrustCoupon',
+    ariaLabel: 'TrustCoupon on Instagram',
+    icon: FaInstagram,
+  },
+  {
+    href: 'https://www.pinterest.com/TrustCoupon',
+    ariaLabel: 'TrustCoupon on Pinterest',
+    icon: FaPinterest,
+  },
+  {
+    href: 'https://x.com/TrustCoupon',
+    ariaLabel: 'TrustCoupon on X',
+    icon: FaXTwitter,
+  },
+  {
+    href: 'https://www.tiktok.com/@TrustCoupon',
+    ariaLabel: 'TrustCoupon on TikTok',
+    icon: FaTiktok,
+  },
+]
+
+const LeftFooter = () => {
+  return (
+    <div className="isolate w-full text-white md:w-2/5">
+      <div className="flex flex-col items-start justify-start gap-6 lg:mb-8">
+        <Link
+          href={APP_ROUTERS.INDEX}
+          className="relative block aspect-auto h-[100px] w-[200px]"
+        >
+          <Image
+            src="/images/logo-with-white-text-and-green-logo.png"
+            alt="Logo"
+            fill
+            priority
+            className="object-contain"
+            sizes="(max-width: 768px) 200px, (max-width: 1200px) 200px, 80px"
+          />
+        </Link>
+        <p className="leading-8">
+          TrustCoupon and RMN are registered trademarks of TrustCoupon, Inc.
+          Third-party trademarks are the property of their respective
+          third-party owners. Presence of a third-party trademark does not mean
+          that TrustCoupon has any relationship with that third-party or that
+          the third-party endorses TrustCoupon or its services.
+        </p>
+        <div className="mb-8 flex gap-4 lg:mb-0 lg:gap-6">
+          {BOTTOM_FOOTER_DATA.map((social, index) => {
+            const IconComponent = social.icon
+            return (
+              <Link
+                key={index}
+                className="flex h-10 w-10 items-center justify-center rounded-full text-white hover:bg-white/10"
+                href={social.href}
+                aria-label={social.ariaLabel}
+              >
+                <IconComponent className="h-6 w-6" />
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LeftFooter
---
+++ src/components/footer/RightFooter.tsx
@@ -0,0 +1,67 @@
+import { APP_ROUTERS } from '@/helpers/config'
+import Link from 'next/link'
+import React from 'react'
+
+const SAMPLE_FOOTER_DATA = {
+  columns: [
+    {
+      title: 'Discover More',
+      links: [
+        { label: 'BROWSE STORES', href: APP_ROUTERS.ALL_CATEGORIES },
+        { label: 'BROWSE CATEGORIES', href: APP_ROUTERS.STORES },
+        { label: 'THE REAL DEAL BLOG', href: APP_ROUTERS.ALL_TOPICS },
+      ],
+    },
+    {
+      title: 'Tools & Support',
+      links: [
+        { label: 'MY TrustCoupon', href: APP_ROUTERS.MY_COUPONS },
+        { label: 'My Account + Rewards', href: APP_ROUTERS.PROFILE },
+        { label: 'Submit a Coupon', href: APP_ROUTERS.SUBMIT_COUPON },
+        { label: 'Get Help', href: '#' },
+      ],
+    },
+  ],
+}
+
+const RightFooter = () => {
+  return (
+    <div className="md:-gap-4 flex flex-1 flex-wrap justify-between px-0 py-8 lg:gap-8 lg:px-8">
+      {SAMPLE_FOOTER_DATA.columns.map((column, index) => (
+        <div key={index} className="flex flex-col gap-8">
+          <b className="text-lg font-bold uppercase">{column.title}</b>
+          {column.links && (
+            <ul className="flex flex-col gap-4">
+              {column.links.map((link, linkIndex) => (
+                <li key={linkIndex}>
+                  <Link
+                    href={link.href}
+                    className="transition-colors hover:border-b"
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
+      <form className="mt-8 w-full">
+        <fieldset className="flex gap-8 overflow-hidden rounded-full bg-white px-4 py-2">
+          <input
+            placeholder="Enter your email"
+            className="w-full bg-white text-black"
+          />
+          <button
+            type="button"
+            className="bg-olive-green -full rounded-full px-4 py-2"
+          >
+            Subscribe
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  )
+}
+
+export default RightFooter
