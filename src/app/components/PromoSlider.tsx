1c951308272c49eed72f638fe4b6dad0dcd83384
tms-nguyenvu <vu.nguyen.tms@gmail.com>
2025-07-14 09:57:47 +0700

    fear/home_page

---
+++ src/app/components/PromoSlider.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import Image from 'next/image'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import Link from 'next/link'
+
+interface PromoSliderProps {
+  banner_id: string
+  banner_title: string
+  banner_image: string[]
+  banner_link: string
+  banner_description: string
+}
+
+export default function PromoSlider({
+  bannerList,
+}: {
+  bannerList: PromoSliderProps[]
+}) {
+  return (
+    <div className="my-4 w-full">
+      <Splide
+        options={{
+          type: 'loop',
+          perPage: 1,
+          perMove: 1,
+          pagination: true,
+          autoplay: true,
+          interval: 2000,
+          pauseOnHover: true,
+          pauseOnFocus: true,
+          arrows: true,
+        }}
+        aria-label="Promo Slider"
+      >
+        {bannerList.map((banner) => (
+          <SplideSlide
+            key={banner.banner_id}
+            className="group mx-auto w-full px-4 pb-4 lg:max-w-[1024px] xl:max-w-[1280px]"
+          >
+            <div className="relative h-[400px] overflow-hidden rounded-2xl md:h-[360px] md:rounded-4xl">
+              <div className="flex h-full w-full justify-between">
+                <Image
+                  src={banner.banner_image[0]}
+                  alt={banner.banner_title}
+                  width={330}
+                  height={360}
+                  className="flex hidden min-h-full w-auto md:block md:w-1/3 md:max-w-[330px]"
+                />
+
+                <picture className="primary-image flex min-h-full w-full md:w-2/3 md:max-w-[660px]">
+                  <Image
+                    src={banner.banner_image[1]}
+                    alt={banner.banner_title}
+                    width={660}
+                    height={360}
+                    className="min-h-full max-w-none min-w-full object-cover"
+                  />
+                </picture>
+              </div>
+              <div className="absolute right-0 bottom-0 left-0 z-10 flex w-full flex-col bg-gradient-to-t from-gray-900 px-6 py-2.5 font-bold text-white md:left-[25%] md:h-[360px] md:w-72 md:bg-black md:bg-none">
+                <h2 className="mt-auto mb-3 text-xs leading-tight tracking-wide uppercase md:mb-5 md:tracking-widest">
+                  {banner.banner_title}
+                </h2>
+                <p className="tracking-none mb-3 text-lg font-semibold md:text-xl md:font-normal md:tracking-normal">
+                  {banner.banner_description}
+                </p>
+                <Link
+                  href={banner.banner_link}
+                  className="mt-4 mb-auto hidden text-xs tracking-widest uppercase underline underline-offset-4 md:block"
+                >
+                  Shop Now
+                </Link>
+              </div>
+            </div>
+          </SplideSlide>
+        ))}
+      </Splide>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 import Footer from '@/components/footer'
 import Header from '@/components/header'
+import PromoSlider from './components/PromoSlider'
 
+const BANNER_LIST = [
+  {
+    banner_id: '1',
+    banner_title: 'Your Bonus is Here',
+    banner_image: ['/images/banner_1.0.webp', '/images/banner_1.1.webp'],
+    banner_link: '/',
+    banner_description: 'Sign up for an account to get your first $5 bonus*',
+  },
+  {
+    banner_id: '2',
+    banner_title: 'Extra 40% Off at Checkout + Free Shipping',
+    banner_image: ['/images/banner_2.0.webp', '/images/banner_2.1.webp'],
+    banner_link: '/',
+    banner_description: 'at Gap',
+  },
+]
 export default function LandingPage() {
   return (
     <>
       <Header />
-      <main className="my-10 px-4">Home</main>
+      <main>
+        <section>
+          <PromoSlider bannerList={BANNER_LIST} />
+        </section>
+      </main>
       <Footer />
     </>
   )
