dc6af12b89ae4867fa35598be9cc0d45f93c6d2b
William Dinh <hoangdinh2411@gmail.com>
2025-07-29 23:40:06 +0200

    integrate api for topic page, fix pagination

--- src/app/(home)/topics/[slug]/page.tsx
+++ src/app/(home)/topics/[slug]/page.tsx
@@ -1,83 +1,88 @@
 import Image from 'next/image'
-import ListBlogs from '../components/ListBlogs'
 import Pagination from '@/components/pagination'
-import { getLatestBlogs } from '@/services/blogApi'
-
-const TOPIC_DATA = {
-  topic_name: 'Beauty',
-  topic_image: '/images/blog-beauty-124x124.webp',
-  topic_description:
-    'Kiss these gorgeous tips hello, and put your best face forward for less.',
-  topic_slug: 'beauty',
-  topic_id: 1,
-  topic_created_at: '2021-01-01',
-  topic_updated_at: '2021-01-01',
-}
-
-const FAKE_PAGINATION = {
-  totalItems: 100,
-  limit: 10,
-}
+import { Fragment } from 'react'
+import TopicList from '@/components/topic/TopicList'
+import Link from 'next/link'
+import { getBlogsByTopic, getTopics } from '@/services/topicApi'
+import { notFound, redirect } from 'next/navigation'
+import { APP_ROUTERS } from '@/helpers/config'
+import ListBlogs from '../../blogs/components/ListBlogs'
 
 export default async function TopicDetailPage({
   params,
+  searchParams,
 }: {
   params: Promise<{ slug: string }>
   searchParams: Promise<{ page?: string }>
 }) {
-  const [latestRes] = await Promise.all([getLatestBlogs()])
   const { slug } = await params
+  const search = await searchParams
+  const page = search.page || 1
+  if (!slug) {
+    redirect(APP_ROUTERS.INDEX)
+  }
+  const limit = 12
+  const [blogsRes, topicRes] = await Promise.all([
+    getBlogsByTopic(slug, +page, limit),
+    getTopics(),
+  ])
 
-  const totalPages = Math.ceil(
-    FAKE_PAGINATION.totalItems / FAKE_PAGINATION.limit,
-  )
-
-  let topic = null
-  if (slug === TOPIC_DATA.topic_slug) {
-    topic = TOPIC_DATA
-  } else {
-    // redirect('/not-found')
+  if (!topicRes.success || !topicRes.data) {
+    throw new Error(topicRes.message ?? 'Cannot fetch topics')
+  }
+  if (!blogsRes.success || !blogsRes.data) {
+    notFound()
   }
 
-  const latest = latestRes.data || []
-  const rowTopic = latest.slice(0, 3)
-
+  const topics = topicRes.data || []
+  const blogs = blogsRes?.data?.results || []
+  const top_blogs = blogs.slice(0, 3)
+  const rest = blogs.slice(3, blogs.length)
+  const total = blogsRes?.data?.total
+  const topic = blogs[0].topic
   return (
-    <div className="mt-10">
-      <div className="mx-auto max-w-[1162px]">
-        <div className="mb-10 flex flex-wrap items-center justify-between">
-          <div className="flex-1">
-            <h1 className="flex items-center gap-4 text-5xl font-bold text-[#ff5c6d] capitalize">
-              <Image
-                className="size-10 rounded-full border-2 border-gray-100"
-                src={topic?.topic_image || '/images/no-img.webp'}
-                alt={topic?.topic_name || ''}
-                width={160}
-                height={160}
-              />
-              {slug}
-            </h1>
-          </div>
-          <p className="text-lg font-normal text-gray-500">
-            {topic?.topic_description}
+    <Fragment>
+      <nav className="mx-auto max-w-[1162px] pt-10">
+        <TopicList topics={topics} />
+        <div className="mt-4 text-center text-xs">
+          <p className="text-olive-green text-xs">
+            Every product and brand is selected by RetailMeNot&apos;s editors.
+            We may earn a commission on the items you choose to buy.{' '}
+            <Link className="font-bold" href="/blogs/about">
+              Learn more
+            </Link>
           </p>
         </div>
-
-        <ListBlogs blogs={rowTopic} type="auto" />
-        <div className="mb-10 flex flex-col gap-[30px] md:flex-row">
-          <div className="flex-1">
-            <ListBlogs blogs={latest} type="row" />
-            <div className="flex items-center justify-center py-5">
-              <Pagination totalPages={totalPages} slug={slug} />
+      </nav>
+      <div className="mt-10">
+        <div className="mx-auto max-w-[1162px]">
+          <div className="mb-10 flex flex-wrap items-center justify-between">
+            <div className="flex-1">
+              <h1 className="flex items-center gap-4 text-5xl font-bold text-[#ff5c6d] capitalize">
+                <Image
+                  className="size-10 rounded-full border-2 border-gray-100"
+                  src={topic?.image.url || '/images/no-img.webp'}
+                  alt={topic?.name || ''}
+                  width={160}
+                  height={160}
+                />
+                {slug}
+              </h1>
             </div>
           </div>
-          <div className="hidden w-full md:w-1/3 lg:block">
-            {/* <Suspense>
-              <TrendingBlogs />
-            </Suspense> */}
+
+          <ListBlogs blogs={top_blogs} type="auto" />
+          <div className="mb-10 flex flex-col gap-[30px] md:flex-row">
+            <div className="flex-1">
+              <ListBlogs blogs={rest} type="row" />
+              <div className="flex items-center justify-center py-5">
+                <Pagination total={total} limit={limit} currentPage={+page} />
+              </div>
+            </div>
+            <div className="hidden w-full md:w-1/3 lg:block"></div>
           </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   )
 }
--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,92 +1,59 @@
 'use client'
-import { useMemo } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 interface PaginationProps {
-  totalPages: number
-  slug: string
+  total: number
+  limit: number
+  currentPage: number
 }
 
-export default function Pagination({ totalPages, slug }: PaginationProps) {
+export default function Pagination({
+  total,
+  limit,
+  currentPage,
+}: PaginationProps) {
   const router = useRouter()
+  const pathname = usePathname()
   const searchParams = useSearchParams()
-  const offset = parseInt(searchParams.get('page') || '1', 10) || 1
+  const list =
+    total && total > limit
+      ? [...Array(Math.ceil(total / limit))].map((_, i) => i + 1)
+      : [1]
 
-  const getPages = () => {
-    const pages: (number | string)[] = []
-    const delta = 1
-
-    if (totalPages <= 5) {
-      for (let i = 1; i <= totalPages; i++) {
-        pages.push(i)
-      }
-    } else {
-      const left = Math.max(2, offset - delta)
-      const right = Math.min(offset + delta, totalPages - 1)
-
-      pages.push(1)
-      if (left > 2) pages.push('...')
-
-      for (let i = left; i <= right; i++) {
-        pages.push(i)
-      }
-
-      if (right < totalPages - 1) pages.push('...')
-      pages.push(totalPages)
-    }
-
-    return pages
+  const handleChangePage = (selectedPage: number) => {
+    const newParams = new URLSearchParams(searchParams)
+    newParams.append('page', selectedPage.toString())
+    router.push(`${pathname}?${newParams.toString()}`)
+    window.scrollTo({ top: 0, behavior: 'smooth' })
   }
-
-  const pages = useMemo(getPages, [offset, totalPages])
-
-  const navigateToPage = (page: number) => {
-    if (page >= 1 && page <= totalPages) {
-      const newParams = new URLSearchParams(searchParams)
-      newParams.set('page', page.toString())
-      router.push(`/topics/${slug}?${newParams.toString()}`)
-      window.scrollTo({ top: 0, behavior: 'smooth' })
-    }
-  }
-
   return (
     <div className="mt-4 flex items-center space-x-2">
       <button
-        onClick={() => navigateToPage(offset - 1)}
-        disabled={offset === 1}
+        onClick={() => handleChangePage(currentPage - 1)}
+        disabled={currentPage === 1}
         className="cursor-pointer rounded border border-[#0998d6] px-4 py-2 font-bold text-[#0988d6] hover:border-[#602d6c] hover:text-[#602d6c] disabled:opacity-50"
         aria-label="Previous page"
       >
         « Prev
       </button>
 
-      {pages.map((page, index) =>
-        typeof page === 'number' ? (
-          <button
-            key={index}
-            onClick={() => page !== offset && navigateToPage(page)}
-            aria-current={page === offset ? 'page' : undefined}
-            className={`rounded px-4 py-2 font-bold ${
-              page === offset
-                ? 'border border-[#a7a7a7] text-[#a7a7a7]'
-                : 'cursor-pointer border border-[#0998d6] text-[#0988d6] hover:border-[#602d6c] hover:text-[#602d6c]'
-            }`}
-          >
-            {page}
-          </button>
-        ) : (
-          <span
-            key={index}
-            className="rounded border border-[#a7a7a7] px-4 py-2 font-bold text-[#a7a7a7]"
-          >
-            {page}
-          </span>
-        ),
-      )}
-
+      {list.map((page) => (
+        <button
+          key={page}
+          onClick={() => handleChangePage(page)}
+          aria-current={page === currentPage ? 'page' : undefined}
+          className={`rounded px-4 py-2 font-bold ${
+            page === currentPage
+              ? 'border border-[#a7a7a7] text-[#a7a7a7]'
+              : 'cursor-pointer border border-[#0998d6] text-[#0988d6] hover:border-[#602d6c] hover:text-[#602d6c]'
+          }`}
+        >
+          {page}
+        </button>
+      ))}
       <button
-        onClick={() => navigateToPage(offset + 1)}
-        disabled={offset === totalPages}
+        onClick={() => handleChangePage(currentPage + 1)}
+        disabled={currentPage === list.length}
         className="cursor-pointer rounded border border-[#0998d6] px-4 py-2 font-bold text-[#0988d6] hover:border-[#602d6c] hover:text-[#602d6c] disabled:opacity-50"
         aria-label="Next page"
       >
