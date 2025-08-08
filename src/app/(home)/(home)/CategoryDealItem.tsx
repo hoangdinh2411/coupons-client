499dfda57129265d27b75d14fd2f583243078637
William Dinh <hoangdinh2411@gmail.com>
2025-08-08 15:02:17 +0200

    fix quality image for cateogory list i home page

--- src/app/(home)/(home)/CategoryDealItem.tsx
+++ src/app/(home)/(home)/CategoryDealItem.tsx
@@ -9,24 +9,20 @@ interface CategoryDealItemProps {
 export default function CategoryDealItem({ category }: CategoryDealItemProps) {
   return (
     <Link
-      href={`/coupons/${category.slug}`}
-      key={category.id}
-      className="group mb-auto flex flex-col items-center justify-between"
+      href={'/coupons/' + category.slug}
+      className={`group flex flex-col items-center justify-center gap-2`}
     >
-      <div className="relative mx-auto mb-3 size-[32px] overflow-hidden rounded-full border border-[#003b95] bg-[#003b95] p-4 transition-shadow duration-300 ease-out group-hover:shadow-xl sm:size-[48px] md:size-[80px] md:p-5">
-        <Image
-          src={category.image?.url ?? '/images/no-img.webp'}
-          alt={category.name}
-          fill
-          priority
-          quality={100}
-          sizes="80px"
-          className="aspect-square h-auto w-full object-contain"
-        />
-      </div>
-      <div className="mx-auto text-center text-xs leading-tight group-hover:underline group-hover:underline-offset-4 lg:text-sm">
-        <p>{category.name}</p>
-      </div>
+      <Image
+        width={100}
+        height={100}
+        alt={category.name}
+        className="h-[100px] w-[100px] rounded-full object-cover"
+        src={`${category?.image?.url ?? '/images/no-img.webp'}`}
+        objectFit="contain"
+      />
+      <p className="text-center text-sm font-bold break-all uppercase group-hover:underline">
+        {category.name}
+      </p>
     </Link>
   )
 }
