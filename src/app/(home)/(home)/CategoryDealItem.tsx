e41082a6e8629cdd8b4c40d52d3dc0f9b8e7cf4f
William Dinh <hoangdinh2411@gmail.com>
2025-08-08 14:54:45 +0200

    change size for image of category list i home page

--- src/app/(home)/(home)/CategoryDealItem.tsx
+++ src/app/(home)/(home)/CategoryDealItem.tsx
@@ -13,14 +13,15 @@ export default function CategoryDealItem({ category }: CategoryDealItemProps) {
       key={category.id}
       className="group mb-auto flex flex-col items-center justify-between"
     >
-      <div className="relative mx-auto mb-3 size-[90px] overflow-hidden rounded-full border border-[#003b95] bg-[#003b95] p-4 transition-shadow duration-300 ease-out group-hover:shadow-xl md:h-32 md:w-32 md:p-5">
+      <div className="relative mx-auto mb-3 size-[32px] overflow-hidden rounded-full border border-[#003b95] bg-[#003b95] p-4 transition-shadow duration-300 ease-out group-hover:shadow-xl sm:size-[48px] md:size-[80px] md:p-5">
         <Image
           src={category.image?.url ?? '/images/no-img.webp'}
           alt={category.name}
           fill
-          loading="lazy"
-          sizes="86px"
-          className="aspect-square h-auto w-full object-cover"
+          priority
+          quality={100}
+          sizes="80px"
+          className="aspect-square h-auto w-full object-contain"
         />
       </div>
       <div className="mx-auto text-center text-xs leading-tight group-hover:underline group-hover:underline-offset-4 lg:text-sm">
